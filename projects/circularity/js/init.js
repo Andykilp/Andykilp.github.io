4c21ec7ebfe9b538e8a072002f4f6b63e5a77d50
Andykilp <andykilpatrick8@gmail.com>
2021-11-09 16:56:35 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -31,11 +31,11 @@ var init = function (window) {
         }
 
         // TODO 3 / 8 : Call the drawCircle() function 
-        drawCircle(); 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
+        
+        for (var circlesDrawn = 0 ; circlesDrawn <= 100; circlesDrawn++){
+            drawCircle();
+            
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -47,19 +47,17 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition( circles[0] );
-            physikz.updatePosition( circles[1] );
-            physikz.updatePosition( circles[2] );
-            physikz.updatePosition( circles[3] );
-            physikz.updatePosition( circles[4] );
+                       // the instructions told me to
+                       
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition( circles[0]);
-            game.checkCirclePosition( circles[1]);
-            game.checkCirclePosition( circles[2]);
-            game.checkCirclePosition( circles[3]);
-            game.checkCirclePosition( circles[4]);
+           // the instructions told me to
             // TODO 9 : Iterate over the array
-           
+            for (var circleUpdate = 0; circleUpdate < circles.length; circleUpdate++) {
+                var eachValue = circles[circleUpdate];
+                var eachCircle = circles.length;
+                physikz.updatePosition(circles[circleUpdate]);
+                game.checkCirclePosition(circles[circleUpdate]);
+            }
             
         }
     
@@ -76,9 +74,15 @@ var init = function (window) {
             }
             
             // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-            
-
-
+            if (circle.x < 0) {
+                circle.x = canvas.width
+            }
+            if (circle.y < 0) {
+                circle.y = canvas.height
+            }
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+            }
             // YOUR TODO 7 CODE ENDS HERE //////////////////////////
         }
         
