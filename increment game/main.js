e3a397ec7df0fb8e9f6a27d4ae27b12bddbd68b7
Andykilp <andykilpatrick8@gmail.com>
2021-12-13 17:35:37 +0000

    add incrementgame

--- increment game/main.js
+++ increment game/main.js
@@ -7,8 +7,11 @@ function whenClicked(number) {
     document.getElementById("clicks").innerHTML = clicks;
 }
 // upgrades code
-
-
+var clicksPerSecond = 0
+function updateClicksPerSecond(numberIncrease) {
+    clicksPerSecond = clicksPerSecond + numberIncrease
+    document.getElementById('clicksPerSecond').innerHTML = clicksPerSecond
+}
 // cursor
 var cursors = 0;
 function buyCursor() {
@@ -18,6 +21,7 @@ function buyCursor() {
             clicks = clicks - cursorCost;                          
             document.getElementById('cursors').innerHTML = cursors;  
             document.getElementById("clicks").innerHTML = clicks;
+            updateClicksPerSecond(1);
         };
         var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       
         document.getElementById('cursorCost').innerHTML = nextCost;
@@ -32,32 +36,25 @@ function buyMonkey() {
             clicks = clicks - monkeyCost;                          
             document.getElementById('monkeys').innerHTML = monkeys;  
             document.getElementById("clicks").innerHTML = clicks;
+            updateClicksPerSecond(5);
         };
         var nextCost = Math.floor(100 * Math.pow(1.1,monkeys));       
         document.getElementById('monkeyCost').innerHTML = nextCost;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-// upgrades for the upgrades so they are not useless
-
-// function firstCursorUpgrade(); {
-//     var cursorUpgradeOne = true;
-// }
-// // check to see if the upgrade should be bought
-// if (cursors >= 10 && cursorUpgradeOne === false) {
-//     document.createElement("p")
-// }
-
+// paperclip
+var paperclips = 0
+function buyPaperclip() {
+        var paperclipCost = Math.floor(1000 * Math.pow(1.1,paperclips));     
+        if(clicks >= paperclipCost){                                   
+            paperclips = paperclips + 1;                                   
+            clicks = clicks - paperclipCost;                          
+            document.getElementById('paperclips').innerHTML = paperclips;  
+            document.getElementById("clicks").innerHTML = clicks;
+            updateClicksPerSecond(20)
+        };
+        var nextCost = Math.floor(1000 * Math.pow(1.1,paperclips));       
+        document.getElementById('paperclipCost').innerHTML = nextCost;
+};
 
 
 
@@ -68,12 +65,12 @@ function buyMonkey() {
 
 // update code
 window.setInterval(function(){
-    whenClicked(cursors)
-    if (cursorUpgradeOne === true) {
-        whenClicked(cursors)
-    }
-    whenClicked(monkeys * 5)
     
-}, 1000);
+    whenClicked(monkeys)
+    whenClicked(paperclips * 4)
+}, 200);
 
+window.setInterval(function(){
+    whenClicked(cursors)
+}, 1000);
 
